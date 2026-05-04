cd0db05def3e280f3704d1a67e0cca3a9a84705c
Bane Sullivan <22067021+banesullivan@users.noreply.github.com>
2026-05-04 08:44:26 -0700

    Use dynamic versioning

--- src/pyvista_miniply/__init__.py
+++ src/pyvista_miniply/__init__.py
@@ -1,13 +1,16 @@
 """pyvista_miniply module."""
 
-from importlib.metadata import PackageNotFoundError, version
-
 from pyvista_miniply.reader import read, read_as_mesh  # noqa: F401
 
 try:
-    __version__ = version("pyvista-miniply")
-except PackageNotFoundError:
-    __version__ = "unknown"
+    from pyvista_miniply._version import version as __version__
+except ImportError:  # pragma: no cover
+    from importlib.metadata import PackageNotFoundError, version
+
+    try:
+        __version__ = version("pyvista-miniply")
+    except PackageNotFoundError:
+        __version__ = "unknown"
 
 
 __all__ = ["read", "read_as_mesh", "__version__"]
